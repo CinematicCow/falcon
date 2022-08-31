fee089b2d1dc824afce721f81a74bd34c8fa538d
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2022-09-01 00:05:24 +0530

    🐞 fix(api): typeorm module
    database connecction fixed

--- libs/api/src/lib/api.module.ts
+++ libs/api/src/lib/api.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from '../database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from '../database/database.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+  ],
   controllers: [],
   providers: [],
   exports: [],
