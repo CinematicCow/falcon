3184e2569e5801937254678d6349325932eebf68
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2022-09-01 00:06:03 +0530

    🦄 refactor(server): add api lib

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { ApiModule } from '@falcon/api';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
+  imports: [ApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
