ec49aad473540e228ef6babc91ea67f002c4fb16
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2022-08-31 23:33:32 +0530

    🦄 refactor(server): rename apps/api
    apps/api renamed to apps/server
    
    api will be used for libs/api

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { ApiCoreModule } from '@falcon/api/core';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ApiCoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
