16e943c2fdecbd79239b3450b8ae2a7d2cc1d63d
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2022-09-02 01:24:33 +0530

    ✨ feat(api/gym): add dto fields
    pass test cases, add validators

--- libs/api/src/gym/dto/create-gym.dto.ts
+++ libs/api/src/gym/dto/create-gym.dto.ts
@@ -1,1 +1,14 @@
-export class CreateGymDto {}
+import { IGym } from '@falcon/interfaces';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateGymDto implements IGym {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsNotEmpty()
+  @IsString()
+  stub: string;
+  @IsNotEmpty()
+  @IsString()
+  location: string;
+}
